fc5ee413169122f9dc32768c0b9f50b68b22648f
shangardevi <shangardevi.natarajan@gmail.com>
2022-05-28 09:42:47 +0530

    adding 3 models

--- data_processing.py
+++ data_processing.py
@@ -1,2 +1,11 @@
 print("adding all the data processing code related to fraud detetion")
 print ("am modle")
+
+def model1():
+    print("adding ML1")
+
+def model2():
+    print("adding ML2")
+
+def model3():
+    print("add 3")
